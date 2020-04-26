2e1960891a8309f14c47b9593b1fae42f7c571f2
Luis J. Villanueva <villanueval@si.edu>
2020-04-25 20:03:47 -0400

    Update napedu.py

--- nap.edu/napedu.py
+++ nap.edu/napedu.py
@@ -10,26 +10,28 @@ feeds = ['http://feeds.nap.edu/nap/new/topic/276', 'http://feeds.nap.edu/nap/new
 
 for feed in feeds:
 	d = feedparser.parse(feed)
-	print("Running: {}".format(d.feed.title_detail['value']))
-	(title, category) = d.feed.title_detail['value'].split("|")
-	title = title.strip()
-	category = category.strip()
-	os.chdir(dest)
-	if os.path.isdir(category) == False:
-		os.mkdir(category)
-	for entry in d.entries:
-		book_title = entry.title_detail['value']
-		entry_link = entry.link.split("/")
-		book_link = entry_link[len(entry_link) - 1]
-		book_file = "{category}/{book_title}.{year}.pdf".format(category = category, book_title = book_title, year = entry['published_parsed'][0])
-		book_url = "https://download.nap.edu/cart/download.cgi?record_id={book_link}".format(book_link = book_link)
-		if os.path.exists(book_file) == False:
-			print("Downloading: {}".format(book_title))
-			p = subprocess.Popen(['wget', '-O', book_file, '--load-cookies', 'cookies.txt', book_url], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			(out,err) = p.communicate()
-			if p.returncode != 0:
-				print("Error: {}\n{}".format(out, err))
-				sys.exit(1)
-		else:
-			print("Skipping: {}".format(book_title))
-
+	try:
+		print("Running: {}".format(d.feed.title_detail['value']))
+		(title, category) = d.feed.title_detail['value'].split("|")
+		title = title.strip()
+		category = category.strip()
+		os.chdir(dest)
+		if os.path.isdir(category) == False:
+			os.mkdir(category)
+		for entry in d.entries:
+			book_title = entry.title_detail['value'].replace("/", "-")
+			entry_link = entry.link.split("/")
+			book_link = entry_link[len(entry_link) - 1]
+			book_file = "{category}/{book_title}.{year}.pdf".format(category = category, book_title = book_title, year = entry['published_parsed'][0])
+			book_url = "https://download.nap.edu/cart/download.cgi?record_id={book_link}".format(book_link = book_link)
+			if os.path.exists(book_file) == False:
+				print("Downloading: {}".format(book_title))
+				p = subprocess.Popen(['wget', '-O', book_file, '--load-cookies', 'cookies.txt', book_url], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+				(out,err) = p.communicate()
+				if p.returncode != 0:
+					print("Error: {}\n{}".format(out, err))
+					sys.exit(1)
+			else:
+				print("Skipping: {}".format(book_title))
+	except:
+		continue
