a86a5d3851cf8c09e3fd0eed02c866242ae71a7f
Luis J. Villanueva <villanueval@si.edu>
2020-05-02 12:29:54 -0400

    Update wget_file.sh

--- wget_file/wget_file.sh
+++ wget_file/wget_file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # wget links in file
 #
-# ./wget_file.sh [txtfile_with_links]
+# ./wget_file.sh [where_to_save] [txtfile_with_links]
 
-wget -c -m --no-cookies -np -r --limit-rate=2m -i $1
+wget -c -m --no-cookies --random-wait -np -r --limit-rate=2m -P $1 -i $2
