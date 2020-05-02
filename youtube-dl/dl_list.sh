a68158f5965d75047acb9d5ac2bc9b0d68de366f
Luis J. Villanueva <villanueval@si.edu>
2020-05-02 18:41:46 -0400

    Update dl_list.sh

--- youtube-dl/dl_list.sh
+++ youtube-dl/dl_list.sh
@@ -4,5 +4,5 @@
 # ./dl_list.sh [ID_of_list]
 
 #Download YouTube playlist videos in separate directory indexed by video order in a playlist
-youtube-dl -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=$1
+youtube-dl --add-metadata --write-info-json --embed-subs --all-subs -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=$1
 
