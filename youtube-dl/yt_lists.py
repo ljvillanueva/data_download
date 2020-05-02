6a1e6149dd53644b383452814a93f926cc82bdf2
Luis J. Villanueva <villanueval@si.edu>
2020-05-02 19:21:32 -0400

    Create yt_lists.py

---
+++ youtube-dl/yt_lists.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#
+# Script to download and update playlists
+
+import os, subprocess, sys, shutil
+
+import settings
+
+playlists = ['PLAbMhAYRuCUhawCEV2oXZGrienoKTN16X', 
+					'PLej_Gvr1c1MM4hEcyzmT06mGoIqoXHcwo']
+
+shutil.copyfile("dl_list.sh", "{}/dl_list.sh".format(settings.dest))
+os.chdir(settings.dest)
+
+for playlist in playlists:
+	try:
+		p = subprocess.Popen(['bash', 'dl_list.sh', playlist], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		(out,err) = p.communicate()
+		if p.returncode != 0:
+			print("Error: {}\n{}".format(out, err))
+			sys.exit(1)
+	except:
+		print("Problem with {}".format(playlist))
+		continue
