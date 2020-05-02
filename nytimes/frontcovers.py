e05b5dbd2b029bbe65f0adbe1b8b70ca28a7ade0
Luis J. Villanueva <villanueval@si.edu>
2020-05-02 17:52:13 -0400

    Update frontcovers.py

--- nytimes/frontcovers.py
+++ nytimes/frontcovers.py
@@ -10,7 +10,7 @@ first = datetime.datetime.strptime("2012-07-06", "%Y-%m-%d")
 current = datetime.datetime.today()
 date_array = (first + datetime.timedelta(days=x) for x in range(0, (current-first).days))
 
-os.mkdir("nytimes")
+os.chdir("nytimes")
 
 for date_object in date_array:
 	cover_url = date_object.strftime("https://static01.nyt.com/images/%Y/%m/%d/nytfrontpage/scannat.pdf")
@@ -18,6 +18,8 @@ for date_object in date_array:
 	file_date = date_object.strftime("%Y-%m-%d")
 	if os.path.isdir("nytimes/{}".format(year_dir)) == False:
 		os.mkdir("nytimes/{}".format(year_dir))
+	if os.isfile("nytimes/{}/{}.pdf".format(year_dir, file_date)):
+		continue
 	print("\nDownloading {} to {}\n".format(cover_url, "nytimes/{}/{}.pdf".format(year_dir, file_date)))
 	try:
 		wget.download(cover_url, "nytimes/{}/{}.pdf".format(year_dir, file_date))
