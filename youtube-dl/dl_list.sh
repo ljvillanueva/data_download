3c86bb4f70d0110793019ccdf0140a291a2cfbdd
Luis J. Villanueva <villanueval@si.edu>
2020-05-03 11:41:51 -0400

    Update dl_list.sh

--- youtube-dl/dl_list.sh
+++ youtube-dl/dl_list.sh
@@ -11,4 +11,4 @@ for i in ${!ytlists[@]}; do
     echo "Working on file ${ytlists[$i]}..."
 	#Download YouTube playlist videos in separate directory indexed by video order in a playlist
 	youtube-dl --add-metadata --write-info-json --embed-subs --all-subs --sub-format "srt" --convert-subs srt --merge-output-format "mkv" -o $destdir'/%(playlist)s/%(playlist_index)s - %(release_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=${ytlists[$i]}
-
+done
