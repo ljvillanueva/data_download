c11c46674d3b9f7f9f308b29e38cbaee6f209d3a
Luis J. Villanueva <villanueval@si.edu>
2020-05-03 11:43:47 -0400

    Update dl_list.sh

--- youtube-dl/dl_list.sh
+++ youtube-dl/dl_list.sh
@@ -8,7 +8,7 @@ source settings.sh
 #Download each file and load it to psql using osm2pgsql
 for i in ${!ytlists[@]}; do
     echo ""
-    echo "Working on file ${ytlists[$i]}..."
+    echo "Working on list ${ytlists[$i]}..."
 	#Download YouTube playlist videos in separate directory indexed by video order in a playlist
-	youtube-dl --add-metadata --write-info-json --embed-subs --all-subs --sub-format "srt" --convert-subs srt --merge-output-format "mkv" -o $destdir'/%(playlist)s/%(playlist_index)s - %(release_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=${ytlists[$i]}
+	youtube-dl --add-metadata --write-info-json --embed-subs --all-subs --sub-format "srt" --convert-subs srt --merge-output-format "mkv" -o $destdir'/%(playlist)s/%(playlist_index)s - %(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=${ytlists[$i]}
 done
