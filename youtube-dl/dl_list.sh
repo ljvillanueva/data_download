a3a4c6476c9e622bab296616a7c0226c43f424bf
Luis J. Villanueva <villanueval@si.edu>
2020-05-03 11:39:42 -0400

    Simplifying dl list

--- youtube-dl/dl_list.sh
+++ youtube-dl/dl_list.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Download youtube list
 #
-# ./dl_list.sh [ID_of_list]
+# 
 
-#Download YouTube playlist videos in separate directory indexed by video order in a playlist
-youtube-dl --add-metadata --write-info-json --embed-subs --all-subs --sub-format "srt" --convert-subs srt --merge-output-format "mkv" -o $1'/%(playlist)s/%(playlist_index)s - %(release_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=$2
+source settings.sh
+
+#Download each file and load it to psql using osm2pgsql
+for i in ${!ytlists[@]}; do
+    echo ""
+    echo "Working on file ${ytlists[$i]}..."
+	#Download YouTube playlist videos in separate directory indexed by video order in a playlist
+	youtube-dl --add-metadata --write-info-json --embed-subs --all-subs --sub-format "srt" --convert-subs srt --merge-output-format "mkv" -o $destdir'/%(playlist)s/%(playlist_index)s - %(release_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=${ytlists[$i]}
 
